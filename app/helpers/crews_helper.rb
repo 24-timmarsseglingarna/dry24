9bc1827d6f0d31d6d13dfdca7cad780a46b364f5
Stefan Pettersson <Stefan Pettersson>
2015-02-23 08:14:32 +0100

    instruments inline, wide screen, help text.

--- app/helpers/crews_helper.rb
+++ app/helpers/crews_helper.rb
@@ -1,2 +1,13 @@
 module CrewsHelper
+
+  def go_section(section)
+
+    raw "Gå till #{section.to_point.number_name} (
+          #{Geocoder::Calculations.compass_point(section.point.bearing_to(section.to_point)).to_s})
+          #{section.distance.to_s} M). <br><span class='defi'>#{section.to_point.definition}</span>"
+  end
+
 end
+
+
+#link_to(go_section(section), create_log_entry_crew_url(:to_point_id => section.to_point.id), :method => :post)
